95b0d0a575f0067a1283eed92fb82875a0580881
Bagelsause <47439839+Bagelsause@users.noreply.github.com>
2020-09-23 17:42:15 -0700

    Update collatzTest.c

--- collatzTest.c
+++ collatzTest.c
@@ -6,11 +6,10 @@ int collatzReturn (int number){
 	while (number != 1) {
 		if (number % 2 == 0) {
 			number /= 2;
-			stoppingTime++;
 		} else {
 			number = (number*3) + 1;
-			stoppingTime++;
 		}
+		stoppingTime++;
 	}
 	return stoppingTime;
 }
