7a3fd5f298f15f16e56456d9cff2328876df114c
Bagelsause <47439839+Bagelsause@users.noreply.github.com>
2021-01-08 17:06:08 -0800

    stats
    keks w

---
+++ stats.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+double sum(const double* nums, size_t count) {
+	double sumValue = 0;
+	for (int i = 0; i < count; i++) {
+		sumValue += nums[i];
+	}
+	
+	return sumValue;
+}
+
+double average(const double* nums, size_t count) {
+	double sumTotal = sum(nums, count);
+	double returned = (sumTotal) / (count);
+	return returned;
+}
+
+double std_dev(const double* nums, size_t count) {
+	double avgValue = average(nums, count);
+	double runningTotal = 0;
+	for (int j = 0; j < count; j++) {
+		double deviation = nums[j] - avgValue;
+		runningTotal += deviation*deviation;
+	}
+	
+	double standardDev = sqrt(runningTotal / count);
+	
+	return standardDev;
+}
+
+int main (int argc, char *argv[]){
+	int num;
+	printf("How many numbers will you enter? ");
+	scanf("%i", &num);
+	double* list = malloc(sizeof(double) * num + sizeof(double));
+	
+	printf("Enter %i numbers, one per line: \n", num);
+	for (int i = 0; i < num; i++){
+		scanf("%lf", &list[i]);
+	}
+	
+	printf("Sum: %lf", sum(list, num));
+	printf("\n Average: %lf", average(list, num));
+	printf("\n Standard Deviation: %lf \n", std_dev(list,num));
+
+	return 0;
+
+}
